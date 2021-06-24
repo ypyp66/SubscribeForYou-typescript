76afa5fd446cf84603bf93b19623873c72dc00a2
ccicci <kongyeesam@gmail.com>
2021-06-25 03:13:16 +0900

    modify subscribe model and add relational files

--- backend/subscribeDatas/models.py
+++ backend/subscribeDatas/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-
-# Create your models here.
-
-
 class SubscribeDatas(models.Model):
-    category = models.CharField(max_length=256, verbose_name='분류', null=True)
-    title = models.CharField(max_length=256, verbose_name='구독서비스 명')
+    user_pk = models.ForeignKey('accounts.User', related_name='user', verbose_name='유저정보', on_delete=models.CASCADE)
+    subs_pk = models.ForeignKey('SubscribeListData', related_name='subs', verbose_name='구독정보', on_delete=models.CASCADE)
+    # category = models.CharField(max_length=256, verbose_name='분류', null=True)
+    # title = models.CharField(max_length=256, verbose_name='구독서비스 명')
     price = models.IntegerField(verbose_name='가격')
     purchaseDay = models.IntegerField(verbose_name='결제일')
-    userid = models.CharField(max_length=256, verbose_name='유저정보')
-    created_date = models.DateTimeField(
-        auto_now_add=True, verbose_name='추가한 날짜')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='추가한 날짜')
 
     def __str__(self):
         return '<%d %s %s>' % (self.pk, self.title, self.userid)
+
+
+class SubscribeListData(models.Model):
+    category = models.CharField(max_length=256, verbose_name='분류')
+    title = models.CharField(max_length=256, verbose_name='구독서비스명')
--- backend/subscribeDatas/views.py
+++ backend/subscribeDatas/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from knox.auth import TokenAuthentication
+
 from .serializers import SubscribeDatasSerializer
 from .models import SubscribeDatas
-# Create your views here.
+from .functions import load_sublist_data
 
 
 class SubscribeDataAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = SubscribeDatasSerializer
 
     def get_queryset(self):
         return SubscribeDatas.objects.all().order_by('id')
 
     def get(self, request, *args, **kwargs):
+        load_sublist_data() # 최초 한번만 실행해야함..... 누군가 해결해줘....
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):

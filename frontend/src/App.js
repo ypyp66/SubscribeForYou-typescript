462efd421cc4551698362cce902e831a3a4a23b7
Yeonbeen <ypyp66@naver.com>
2021-06-27 17:05:19 +0900

    컴포넌트 조건부 렌더링

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,30 @@
-import Home from './routes/Home';
-import { Route, Switch, Router } from 'react-router-dom';
-import Auth from './routes/Auth';
-import Test from './components/Test';
+import { Route, Switch, Router, useHistory, Redirect } from 'react-router-dom';
+import HomeRoute from './routes/HomeRoute';
 import Login from './components/Login';
 import Register from './components/Register';
 import { connect } from 'react-redux';
+import HomeContainer from './containers/HomeContainer';
 
 function App({ user }) {
+  const history = useHistory();
   const authenticated = user != null;
   console.log(user);
 
   return (
     <div className="container mx-auto p-10 h-screen">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route path="/test" component={Test} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/">
+          {authenticated ? <HomeContainer /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          {!authenticated ? <Login /> : <Redirect to="/" />}
+        </Route>
+        <Route exact path="/register">
+          {!authenticated ? <Register /> : <Redirect to="/" />}
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default connect((state) => ({
-  user: state.auth.user,
-  token: state.auth.token,
-}))(App);
+export default App;

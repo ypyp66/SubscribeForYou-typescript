36bc86ed7851abd374050c87f89992ca0fa5496f
HongBi <ksw97482@gmail.com>
2021-07-06 02:15:16 +0900

    add reset password

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 from rest_framework import urls, routers
 from knox import views as knox_views
-from .api import LoginAPI, UserAPI
+from .api import LoginAPI, UserAPI 
+
 
 
 urlpatterns = [
@@ -14,5 +15,5 @@ urlpatterns = [
     path('api/auth', include('knox.urls')),
     path('api/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
     path('api/password_reset', include('django_rest_passwordreset.urls'), name='password_reset'),
-    # path('api/password_reset/confirm', ResetPwdAPI.as_view(), name='password_reset_confirm'),
+    #path('api/password_reset/confirm', ResetPwdAPI.as_view(), name='password_reset_confirm'),
 ]

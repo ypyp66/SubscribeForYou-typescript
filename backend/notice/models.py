8e191af3fa7a3288a2989a251ac4dbc63c788b7f
peeinnkim <peeinnkim@gmail.com>
2021-07-07 23:45:32 +0900

    fix notice model name

--- backend/notice/models.py
+++ backend/notice/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Notice(models.Model):
-    title = models.CharField(verbose_name='제목', null=False, blank=False)
+    title = models.CharField(verbose_name='제목', max_length=255, null=False, blank=False)
     content = models.TextField(verbose_name='내용', null=False, blank=False)
-    user_pk = models.ForeignKey('accounts.User', db_column='user_pk', related_name='user', verbose_name='글쓴이', on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.User', db_column='user_pk', related_name='author', verbose_name='글쓴이', on_delete=models.CASCADE)
     created = models.DateTimeField(verbose_name='작성날짜', auto_now_add=True)
 
     def __str__(self):

6f5758ee1889e933754f7784977da4e4305e1b9a
peeinnkim <peeinnkim@gmail.com>
2021-07-10 19:39:13 +0900

    fix typo

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/user/', UserRegisterAPI.as_view()),
     path('api/user/<int:user_pk>', UserAPI.as_view()),
     path('api/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
-    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/logoutall', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('api/password_reset', include('django_rest_passwordreset.urls'), name='password_reset'),
     path('api/auth', include('knox.urls')),
     path('api/test', views.test, name="test"),

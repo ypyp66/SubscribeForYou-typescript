fd2ced3925f1d0b8e10300af09e92b055b49d565
HongBi <ksw97482@gmail.com>
2021-07-11 23:10:19 +0900

    notice models.py add updated field

--- backend/accounts/permissions_REMOTE_3296.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-from .models import User
-
-class IsOwner(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        
-        return request.user.pk == view.kwargs['user_pk']
--- backend/notice/serializers.py
+++ backend/notice/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Notice
 

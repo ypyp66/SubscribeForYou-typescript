09132638761717bedb3af037b9034122a10c0311
kitebeen <ypyp66@naver.com>
2021-07-14 00:34:47 +0900

    before merge

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -27,7 +27,6 @@ function Home({ post }) {
         ))
       ) : (
         <div className="border rounded-xl bg-gray-100 p-3 mb-3 hover:border-blue-300 w-full focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-          
           <div
             className="cursor:pointer border rounded-xl bg-gray-100 p-6 mb-3 hover:border-blue-300 w-full focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
             onClick={() => {
@@ -37,9 +36,7 @@ function Home({ post }) {
           >
             <div className="w-3/6 m-0">
               <div className="text-xl">유튜브 프리미엄</div>
-              <div className="text-sm">
-                매달 1일, 3,500원
-              </div>
+              <div className="text-sm">매달 1일, 3,500원</div>
             </div>
           </div>
           <div
@@ -51,9 +48,7 @@ function Home({ post }) {
           >
             <div className="w-3/6 m-0">
               <div className="text-xl">넷플릭스</div>
-              <div className="text-sm">
-                매달 30일, 35,000원
-              </div>
+              <div className="text-sm">매달 30일, 35,000원</div>
             </div>
           </div>
           <div
@@ -65,9 +60,7 @@ function Home({ post }) {
           >
             <div className="w-3/6 m-0">
               <div className="text-xl">라프텔</div>
-              <div className="text-sm">
-                매달 15일, 32,340원
-              </div>
+              <div className="text-sm">매달 15일, 32,340원</div>
             </div>
           </div>
           <div
@@ -79,9 +72,7 @@ function Home({ post }) {
           >
             <div className="w-3/6 m-0">
               <div className="text-xl">넷플릭스</div>
-              <div className="text-sm">
-                매달 30일, 35,000원
-              </div>
+              <div className="text-sm">매달 30일, 35,000원</div>
             </div>
           </div>
           <div
@@ -93,16 +84,10 @@ function Home({ post }) {
           >
             <div className="w-3/6 m-0">
               <div className="text-xl">넷플릭스</div>
-              <div className="text-sm">
-                매달 30일, 35,000원
-              </div>
+              <div className="text-sm">매달 30일, 35,000원</div>
             </div>
           </div>
-
-
         </div>
-
-
       )}
       {!post && <div>로딩중</div>}
       <div className="flex justify-end z-30 -mt-11">
